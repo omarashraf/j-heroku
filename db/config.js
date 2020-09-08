d66553deba4324e8ad1244f99dc7187cdc1dd018
Omar <o.abouzaid93@gmail.com>
2020-09-09 00:37:55 +0200

    Update db connection
    ;
    ;

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const env = dotenv.config();
 
-mongoose.connect(`mongodb://${process.env.USER}:${process.env.PASSWORD}@${process.env.DATABASE_URL}`, { useNewUrlParser: true });
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.DATABASE_URL}`, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     console.log("connected to db");
 }, (err) => {
